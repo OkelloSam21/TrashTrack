f3042f24d128ec1aba54798585eb04b0a4cc5cf8
Samuel Okello <samuelokello929@gmail.com>
2024-05-17 06:00:07 +0300

    enhancement make date picker textfield read only and change color icon to seed

--- app/src/main/java/com/samuelokello/trashtrack/ui/components/DatePicker.kt
+++ app/src/main/java/com/samuelokello/trashtrack/ui/components/DatePicker.kt
@@ -1,6 +1,7 @@
 package com.samuelokello.trashtrack.ui.components
 import android.app.DatePickerDialog
 import android.content.Context
+import android.util.Log
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CalendarToday
 import androidx.compose.material3.Icon
@@ -10,26 +11,40 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.samuelokello.trashtrack.ui.theme.seed
 import java.util.Calendar
 
 @Composable
 fun DatePicker() {
+    val TAG = "DATE PICKER"
     var date by remember { mutableStateOf("") }
     val context = LocalContext.current
     TextFieldComponent(
         value = date,
-        onValueChange = { date = it },
-        placeholder = "Select Date"
-    ) {
-        IconButton(onClick = {
-            showDatePicker(context) { selectedDate ->
-                date = selectedDate
+        onValueChange = { date = it
+            Log.e(TAG, "DatePicker: $it", )},
+        placeholder = "Select Date",
+        trailingIcon = {
+            IconButton(onClick = {
+                showDatePicker(context) { selectedDate ->
+                    date = selectedDate
+                }
+            }) {
+                Icon(
+                    Icons.Default.CalendarToday,
+                    contentDescription = "Calendar Icon",
+                    tint = seed
+                )
             }
-        }) {
-            Icon(Icons.Default.CalendarToday, contentDescription = "Calendar Icon")
-        }
-    }
+        },
+        singleLine = true,
+//        isEnable = false,
+        isReadOnly = true
+    )
+
 }
 
 fun showDatePicker(context: Context, onDateSelected: (String) -> Unit) {
@@ -41,4 +56,10 @@ fun showDatePicker(context: Context, onDateSelected: (String) -> Unit) {
         val selectedDate = "$dayOfMonth/${monthOfYear + 1}/$selectedYear"
         onDateSelected(selectedDate)
     }, year, month, day).show()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DatePickerPrev(modifier: Modifier = Modifier) {
+    DatePicker()
 }
